4778669072e7aa45e20be34a04726b2df1e5e04f
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-04-06 18:30:25 +0100

    Fixing some errors

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
-/**
- * Module dependencies.
- */
-
+/*
+Module Dependencies 
+*/
 var express = require('express'),
     http = require('http'),
     path = require('path'),
@@ -11,19 +9,18 @@ var express = require('express'),
 
 var app = express();
 
-/** 
- * Database and Models 
- */
-mongoose.connect("mongodb://localhost/users");
-var userSchema = new mongoose.Schema({
-	username: String,
+/*
+Database and Models
+*/
+mongoose.connect("mongodb://localhost/myapp");
+var UserSchema = new mongoose.Schema({
+    username: String,
     password: String,
     salt: String,
     hash: String
 });
 
-var User = mongoose.model('users', userSchema);
-
+var User = mongoose.model('users', UserSchema);
 /*
 Middlewares and configurations 
 */
@@ -36,9 +33,6 @@ app.configure(function () {
     app.set('view engine', 'jade');
 });
 
-/**
- * Success and Error messages
- */
 app.use(function (req, res, next) {
     var err = req.session.error,
         msg = req.session.success;
@@ -49,38 +43,29 @@ app.use(function (req, res, next) {
     if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
     next();
 });
+/*
+Helper Functions
+*/
+function authenticate(name, pass, fn) {
+    if (!module.parent) console.log('authenticating %s:%s', name, pass);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+    User.findOne({
+        username: name
+    },
 
-
-/**
- * Helper functions
- */
-function authenticate(name, password, fn) {
-	if (!module.parent) 
-		console.log('authenticating %s:%s', name, password);
-
-	User.findOne({
-		username: name
-	},  function (err, user) {
-        	if (user) {
-            	if (err) 
-            		return fn(new Error('cannot find user'));
-            		
-            		hash(pass, user.salt, function (err, hash) {
-                		if (err) 
-                			return fn(err);
-                		if (hash == user.hash) 
-                			return fn(null, user);
-                		fn(new Error('invalid password'));
-            		});
-        	} else {
-            	return fn(new Error('cannot find user'));
-        	}
+    function (err, user) {
+        if (user) {
+            if (err) return fn(new Error('cannot find user'));
+            hash(pass, user.salt, function (err, hash) {
+                if (err) return fn(err);
+                if (hash == user.hash) return fn(null, user);
+                fn(new Error('invalid password'));
+            });
+        } else {
+            return fn(new Error('cannot find user'));
+        }
     });
+
 }
 
 function requiredAuthentication(req, res, next) {
@@ -105,9 +90,9 @@ function userExist(req, res, next) {
     });
 }
 
-/**
- * Routes
- */  
+/*
+Routes
+*/
 app.get("/", function (req, res) {
 
     if (req.session.user) {
@@ -182,7 +167,4 @@ app.get('/profile', requiredAuthentication, function (req, res) {
 });
 
 
-/**
- * Create app and listen on port 3000
- */
- http.createServer(app).listen(3000);
+http.createServer(app).listen(3000);
